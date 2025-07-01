7b682656f13f9634054ccf70fef85f5b30f8b1e5
RobinsAviary <robinsaviary@gmail.com>
2025-07-01 09:23:04 -0400

    basic pointer

--- include/Objs/Pointer.lua
+++ include/Objs/Pointer.lua
@@ -1,5 +1,10 @@
 Pointer = Obj:new({
     si = 1,
+    sw = 2,
+    sh = 2,
+    spd = .65,
+    ox = 5,
+    oy = 5,
 
     new = function(self, tbl)
         tbl = Obj.new(self, tbl)
@@ -10,6 +15,9 @@ Pointer = Obj:new({
     end,
 
     step=function(self)
+        self.xspd = Input.x * self.spd
+        self.yspd = Input.y * self.spd
+
         self:move()
     end,
 
--- include/main.lua
+++ include/main.lua
@@ -6,5 +6,6 @@ end
 
 function _update60()
     cls(0)
+    updateInput()
     iteratecollection(Objs)
 end
